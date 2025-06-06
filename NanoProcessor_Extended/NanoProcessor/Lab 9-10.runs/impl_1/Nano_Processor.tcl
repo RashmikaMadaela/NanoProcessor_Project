7993f2d7cadeac799b4d945d3b8185c1420efadc
Rashmika Madaela <rashmikamadaelaofficial@gmail.com>
2025-06-06 21:28:44 +0530

    Working - Bitstream Generated

--- NanoProcessor_Extended/NanoProcessor/Lab 9-10.runs/impl_1/Nano_Processor.tcl
+++ NanoProcessor_Extended/NanoProcessor/Lab 9-10.runs/impl_1/Nano_Processor.tcl
@@ -66,7 +66,7 @@ set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
   open_checkpoint Nano_Processor_routed.dcp
-  set_property webtalk.parent_dir {C:/Users/DOWNLOAD/Downloads/NanoProcessor_Extended/NanoProcessor/Lab 9-10.cache/wt} [current_project]
+  set_property webtalk.parent_dir {E:/Github/NanoProcessor_Project/NanoProcessor_Extended/NanoProcessor/Lab 9-10.cache/wt} [current_project]
   catch { write_mem_info -force Nano_Processor.mmi }
   write_bitstream -force Nano_Processor.bit 
   catch {write_debug_probes -quiet -force Nano_Processor}
--- NanoProcessor_Extended/NanoProcessor/Lab 9-10.runs/impl_1/runme.sh
+++ NanoProcessor_Extended/NanoProcessor/Lab 9-10.runs/impl_1/runme.sh
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='C:/Users/DOWNLOAD/Downloads/NanoProcessor_Extended/NanoProcessor/Lab 9-10.runs/impl_1'
+HD_PWD='E:/Github/NanoProcessor_Project/NanoProcessor_Extended/NanoProcessor/Lab 9-10.runs/impl_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
